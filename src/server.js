20f09abbae8f39b4200dd2ab5dee93d913b6d848
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-06-11 11:35:05 -0300

    configuraçoes do sequelize para conexao com o postgres e arquivo de variaveis de ambiente.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
+require('dotenv/config');
 const express = require('express');
 
 const routes = require('./routes');
 
 const app = express();
 
-app.use(routes);
 app.use(express.json());
+app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT);
